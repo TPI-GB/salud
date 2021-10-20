58202e6526644c68ecd8b3165ef216a358524c07
Tomas Trinchero <trinchero52@gmail.com>
2021-10-20 19:02:07 -0300

    levantando base servidor

--- salud-backend/app.js
+++ salud-backend/app.js
@@ -3,6 +3,8 @@ require("./config/database").connect();
 const express = require("express");
 const app = express();
 
+const port = process.env.port || 3001
+
 app.use(express.json());
 
 // Logic goes here
