c6e39fea93fd18ffa6dc5b53fe2f4f7616e7108a
RoBassi <89803869+RoBassi@users.noreply.github.com>
2021-11-01 19:10:22 -0300

    ruteo basico

---
+++ salud-frontend/src/components/BuscarPaciente/BuscarPaciente.jsx
@@ -0,0 +1,10 @@
+import "./BuscarPaciente.scss";
+import React from 'react'
+
+const BuscarPaciente = () => (
+  <section className="BuscarPaciente">
+    <h3>Esto es Buscar Paciente</h3>
+  </section>
+)
+
+export default BuscarPaciente;
