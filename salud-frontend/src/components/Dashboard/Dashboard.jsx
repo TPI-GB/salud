530028db7511e1f7a91c7c33128f64b87a89db8d
Juan Hiribarren <juanelvasco@gmail.com>
2021-11-04 18:19:51 -0300

    mergeo con main

--- salud-frontend/src/components/Dashboard/Dashboard.jsx
+++ salud-frontend/src/components/Dashboard/Dashboard.jsx
@@ -26,31 +26,35 @@ export default function Dashboard(props) {
         <Grid item xs={12}>
           <Navbar />
         </Grid>
-          <CardHeader icono={<StackedBarChartIcon className={classes.iconos}/>} titulo='Cantidad Semanal de Pacientes' texto="Muestra la cantidad de pacientes que se atendieron esta semana" color='red'/>
+          <CardHeader icono={<StackedBarChartIcon className={classes.iconos}/>} 
+                titulo='Cantidad Semanal de Pacientes' texto="Muestra la cantidad de pacientes/semana" color='red'/>
         <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
                   
         </Grid>
-        <CardHeader icono={<DonutSmallIcon className={classes.iconos}/>} titulo='Porcentaje de patologias' texto="Muestra las patologias mas comunes" color='red'/>
+          <CardHeader icono={<DonutSmallIcon className={classes.iconos}/>} 
+                titulo='Porcentaje de patologias' texto="Muestra las patologias mas comunes" color='red'/>
         <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
                   
         </Grid>
-        <CardHeader icono={<ShowChartIcon className={classes.iconos}/>} titulo='Cantidad semanal de medicos logeados' texto="Muestra la cantidad semanal de medicos logeados" color='red'/>
+          <CardHeader icono={<ShowChartIcon className={classes.iconos}/>} 
+                titulo='Cantidad semanal de medicos logeados' texto="Muestra la cantidad semanal de medicos logeados" color='red'/>
         <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
                   
         </Grid>
-        {/* <Grid container spacing={1} className={classes.container}>
+        <Grid container spacing={1} className={classes.container}>
           <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-                  
+                <CardHeader icono={<ShowChartIcon className={classes.iconos}/>}  titulo='1' texto="1" color='red'/>
           </Grid>
           <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-                  
+                <CardHeader icono={<ShowChartIcon className={classes.iconos}/>}  titulo='2' texto="2" color='red'/>  
           </Grid>
           <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-                  
+                <CardHeader icono={<ShowChartIcon className={classes.iconos}/>}  titulo='3' texto="3" color='red'/>
           </Grid>
           <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-                  
-          </Grid> */}
+                <CardHeader icono={<ShowChartIcon className={classes.iconos}/>}  titulo='4' texto="4" color='red'/>
+          </Grid>
+        </Grid>
       </Grid>
     </div>
   );
