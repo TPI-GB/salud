24f324b6a26ec6bff25122592ed326ee4098359b
Martin Fondovila <fondovila.martin@gmail.com>
2022-03-10 13:48:17 -0300

    Lista de consultas casi lista

--- salud-frontend/src/components/MedicalConsultationsList/MedicalConsultationList.jsx
+++ salud-frontend/src/components/MedicalConsultationsList/MedicalConsultationList.jsx
@@ -1,27 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Grid,
   Typography,
-  Paper,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListSubheader,
-  Divider,
+  ListItemText,
+  Collapse,
+  ListItemButton,
+  Tooltip,
+  Fab,
 } from "@mui/material";
+import { Link } from "react-router-dom";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
 import MasksIcon from "@mui/icons-material/Masks";
+import AddIcon from "@mui/icons-material/Add";
 
 export default function MedicalConsultationList(props) {
   const { consultas } = props;
 
   return (
     <Box>
-      <List subheader={<ListSubheader>Consultas</ListSubheader>}>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h6">Consultas</Typography>
+        <Tooltip title="Agregar consulta">
+          <Fab
+            size="small"
+            sx={{ ml: "15px" }}
+            component={Link}
+            to={"/NuevaHistoriaClinica"}
+          >
+            <AddIcon />
+          </Fab>
+        </Tooltip>
+      </Box>
+      <Grid container>
         {consultas.map((consulta) => {
           return <ConsultationItemList consultaInfo={consulta} />;
         })}
-      </List>
+      </Grid>
     </Box>
   );
 }
@@ -29,14 +50,68 @@ export default function MedicalConsultationList(props) {
 function ConsultationItemList(props) {
   const { consultaInfo } = props;
 
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
-    <Paper>
-      <ListItem>
-        <ListItemIcon>
-          <MasksIcon />
-        </ListItemIcon>
-        {consultaInfo.doctor}
-      </ListItem>
-    </Paper>
+    <Grid
+      container
+      item
+      sx={{
+        border: "solid 4px green",
+        margin: "10px 0px",
+      }}
+    >
+      <Grid
+        item
+        xs={12}
+        sm={12}
+        md={4}
+        lg={4}
+        xl={4}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center",
+          backgroundColor: "#F2F3F4",
+        }}
+      >
+        <Box>
+          <MasksIcon fontSize="large" />
+          <Typography>{consultaInfo.creacion}</Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
+        <Box textAlign={"center"}>
+          <Typography fontWeight={"bold"}>Doctor</Typography>
+          <Typography backgroundColor={"#F2F3F4"}>
+            {consultaInfo.doctor}
+          </Typography>
+          <Typography fontWeight={"bold"}>Motivo</Typography>
+          <Typography backgroundColor={"#F2F3F4"}>
+            {consultaInfo.motivo}
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+        <Box>
+          <ListItemButton onClick={handleClick}>
+            <ListItemText primary="Descripción" />
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Typography padding={"16px 16px"} backgroundColor={"#F2F3F4"}>
+              {consultaInfo.textoLibre
+                ? consultaInfo.textoLibre
+                : "No se encontró información"}
+            </Typography>
+          </Collapse>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
--- salud-frontend/src/components/MedicalHistoryData/MedicalHistoryData.jsx
+++ salud-frontend/src/components/MedicalHistoryData/MedicalHistoryData.jsx
@@ -5,50 +5,47 @@ export default function MedicalHistoryData(props) {
   const { data } = props;
 
   return (
-    <Box>
+    <Box sx={{ marginBottom: "15px" }}>
       <Grid container spacing={2}>
         <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
           <DataView
-            field="Número de historia clínica: "
+            field="Número de historia clínica"
             info={data.numeroHistoriaClinica}
           />
         </Grid>
         <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
           <DataView
-            field="Nombre y apellido: "
+            field="Nombre y apellido"
             info={data.nombre + " " + data.apellido}
           />
         </Grid>
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <DataView field="País de nacimiento: " info={data.paisDeNacimiento} />
+          <DataView field="País de nacimiento" info={data.paisDeNacimiento} />
         </Grid>
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <DataView
-            field={data.tipoDocumento + ": "}
-            info={data.numeroDocumento}
-          />
+          <DataView field={data.tipoDocumento} info={data.numeroDocumento} />
         </Grid>
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <DataView field="Sexo: " info={data.sexo} />
+          <DataView field="Sexo" info={data.sexo} />
         </Grid>
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <DataView field="Ocupación: " info={data.ocupacionActual} />
+          <DataView field="Ocupación" info={data.ocupacionActual} />
         </Grid>
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <DataView field="Edad: " info={data.edad + " años"} />
+          <DataView field="Edad" info={data.edad + " años"} />
         </Grid>
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <DataView field="Estado civil: " info={data.estadoCivil} />
+          <DataView field="Estado civil" info={data.estadoCivil} />
         </Grid>
         <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-          <DataView field="Domicilio: " info={data.domicilioActual} />
+          <DataView field="Domicilio" info={data.domicilioActual} />
         </Grid>
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <DataView field="Creación: " info={data.creacion} />
+          <DataView field="Creación" info={data.creacion} />
         </Grid>
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
           <DataView
-            field="Ultima modificación: "
+            field="Ultima modificación"
             info={data.ultimaModificacion}
           />
         </Grid>
@@ -61,11 +58,11 @@ function DataView(props) {
   const { field, info } = props;
 
   return (
-    <Paper sx={{ textAlign: "center" }}>
-      <Typography>
-        {field}
-        {info ? info : "No se encotró información"}
-      </Typography>
-    </Paper>
+    <Box sx={{ textAlign: "center" }}>
+      <Typography fontWeight={"bold"}>{field}</Typography>
+      <Paper sx={{ backgroundColor: "#F2F3F4" }}>
+        <Typography>{info ? info : "No se encotró información"}</Typography>
+      </Paper>
+    </Box>
   );
 }
