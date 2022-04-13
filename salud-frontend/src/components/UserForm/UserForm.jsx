65593b1680312b6ad4d8107c91369b76469e5c12
trinchero52 <trinchero52@gmail.com>
2022-04-13 20:23:17 -0300

    editando y creando funcionando

--- salud-frontend/src/components/UserForm/UserForm.jsx
+++ salud-frontend/src/components/UserForm/UserForm.jsx
@@ -24,32 +24,148 @@ export default function UserForm(props) {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({ mode: "onBlur" });
   const onSubmit = async (data) => {
     console.log("onSubmit");
-    await verificarContrasenias(data);
+    //var verificoContrasena = await verificarContrasenias(data);
+    //var verificoMail = await verificarEmail(data);
+    // var verificoTipoDocumento = await verificarTipoDocumento(data);
+    // var verificoNumeroDocumento = await verificarNumeroDocumento(data);
+    // var verificoNombre = await verificarNombre(data);
+    // var verificoApellido = await verificarApellido(data);
+    console.log(!(await verificarContrasenias(data)));
+    if (
+      await verificarContrasenias(data)
+      //&&
+      //verificoMail
+      // &&
+      // verificoTipoDocumento &&
+      // verificoNumeroDocumento &&
+      // verificoNombre &&
+      // verificoApellido
+    ) {
+      let response = id ? await updateUser(id, data) : await createUser(data);
+      if (id) {
+        updateUser(id, data)
+          .then(() => {
+            mostrarAlertaConfirmacion();
+            history.push("/MostrarUsuarios");
+          })
+          .catch(() => mostrarAlertaError());
+      } else {
+        createUser(data)
+          .then(() => {
+            mostrarAlertaConfirmacion();
+            history.push("/MostrarUsuarios");
+          })
+          .catch(() => mostrarAlertaError());
+      }
+    }
   };
-  const [probar, setProbar] = useState({ show: false, message: "" });
+
+  const [mensajeContrasenia, setMensajeContrasenia] = useState({
+    show: false,
+    message: "",
+  });
+  const [mensajeEmail, setMensajeEmail] = useState({
+    show: false,
+    message: "",
+  });
+  const [mensajeTipoDocumento, setMensajeTipoDocumento] = useState({
+    show: false,
+    message: "",
+  });
+  const [mensajeNumeroDocumento, setMensajeNumeroDocumento] = useState({
+    show: false,
+    message: "",
+  });
+  const [mensajeNombre, setMensajeNombre] = useState({
+    show: false,
+    message: "",
+  });
+  const [mensajeApellido, setMensajeApellido] = useState({
+    show: false,
+    message: "",
+  });
+
   async function verificarContrasenias(data) {
-    var p1 = document.getElementById("contraseniaid1").value;
-    var p2 = document.getElementById("confirmContrasenia").value;
-    if (p1 !== p2) {
-      setProbar({ show: true, message: "Contraseñas no concuerdan" });
+    var contra1 = document.getElementById("contraseniaid1").value;
+    var contra2 = document.getElementById("confirmContrasenia").value;
+    if (contra1 !== contra2) {
+      setMensajeContrasenia({
+        show: true,
+        message: "Contraseñas no concuerdan",
+      });
+      return false;
     } else {
-      setProbar({ show: false, message: "" });
-      let response = await createUser(data);
-      if (response === 0) {
-        mostrarAlertaConfirmacion();
-        history.push("/MostrarUsuarios");
-      } else mostrarAlertaError();
+      setMensajeContrasenia({ show: false, message: "" });
+      return true;
     }
   }
+  //FALTAN DESARROLLAR ESTAS FUNCIONES PARA LOS CAMPOS REQUERIDOS USANDO REGEX EXPRESIONES REGULARES QUE ME DIJO FEDE
+
+  // async function verificarEmail(data) {
+  //   // value: true,
+  //   // message: "Necesitas este campo",
+  //   // pattern: {
+  //   //   value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+  //   //   message: "El formato no es correcto",
+  //   // },
+  //   // var campoDeMail = document.getElementById("emailid").value;
+  //   if (campoDeMail == null) {
+  //     setMensajeEmail({
+  //       show: true,
+  //       mensaje: "Necesitas este campo",
+  //     });
+  //   } else {
+  //     setMensajeEmail({ show: false, mensaje: "" });
+  //   }
+  //   if (!campoDeMail.test(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i)) {
+  //     setMensajeEmail({
+  //       show: true,
+  //       mensaje: "El formato no es correcto",
+  //     });
+  //   } else {
+  //     setMensajeEmail({ show: false, mensaje: "" });
+  //   }
+  // }
+
+  async function verificarTipoDocumento(data) {}
+
+  async function verificarNumeroDocumento(data) {}
+
+  async function verificarNombre(data) {}
+
+  async function verificarApellido(data) {}
+
   const [email, setEmail] = useState("");
+  const [tipodocumento, setTipoDocumento] = useState("");
+  const [numerodocumento, setNumeroDocumento] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [roles, setRoles] = useState({
+    medico: false,
+    admin: false,
+    secretaria: false,
+    director: false,
+    laboratorio: false,
+    recepcion: false,
+  });
+  const [contrasenia, setContrasenia] = useState("");
+
   //falta todos los atributos
 
   const mostrarAlertaConfirmacion = () => {
     swal({
       title: "Creación exitosa",
+      icon: "success",
+      button: "Aceptar",
+    });
+  };
+
+  const mostrarAlertaEdicion = () => {
+    swal({
+      title: "Edición exitosa",
       icon: "success",
       button: "Aceptar",
     });
@@ -66,12 +182,25 @@ export default function UserForm(props) {
   useEffect(() => {
     const getData = async () => {
       const response = await getUserById(id);
-      setEmail(response.data.email);
+      setEmail(response.data.email); //falta poner los sets aca
+      setTipoDocumento(response.data.tipodocumento);
+      setNumeroDocumento(response.data.numerodocumento);
+      setNombre(response.data.nombre);
+      setApellido(response.data.apellido);
+      setContrasenia(response.data.contrasenia);
+      setRoles({
+        medico: response.data.roles.includes("medico"),
+        admin: response.data.roles.includes("admin"),
+        secretaria: response.data.roles.includes("secretaria"),
+        director: response.data.roles.includes("director"),
+        laboratorio: response.data.roles.includes("laboratorio"),
+        recepcion: response.data.roles.includes("recepcion"),
+      });
     };
     if (id) {
       getData();
     }
-  }, [id]);
+  }, []);
 
   return (
     <FormControl onSubmit={handleSubmit(onSubmit)}>
@@ -96,7 +225,6 @@ export default function UserForm(props) {
                   },
                   onChange: (event) => {
                     setEmail(event.target.value);
-                    console.log(email);
                   },
                 })}
                 error={Boolean(errors.email)}
@@ -109,10 +237,15 @@ export default function UserForm(props) {
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
                     defaultValue="DNI"
+                    value={tipodocumento}
                     {...register("tipodocumento", {
                       required: {
                         value: true,
                         message: "Necesitas este campo",
+                      },
+                      onChange: (event) => {
+                        setTipoDocumento(event.target.value);
+                        console.log(tipodocumento);
                       },
                     })}
                     error={Boolean(errors.tipodocumento)}
@@ -134,6 +267,7 @@ export default function UserForm(props) {
                     id="numDocid"
                     type="text"
                     label="Numero de documento"
+                    value={numerodocumento}
                     {...register("numerodocumento", {
                       required: {
                         value: true,
@@ -142,6 +276,9 @@ export default function UserForm(props) {
                       pattern: {
                         value: /^[1-9][0-9]{6,8}$/i,
                         message: "El formato no es correcto",
+                      },
+                      onChange: (event) => {
+                        setNumeroDocumento(event.target.value);
                       },
                     })}
                     error={Boolean(errors.numerodocumento)}
@@ -155,10 +292,15 @@ export default function UserForm(props) {
                 id="nombreid"
                 type="text"
                 label="Nombre"
+                value={nombre}
                 {...register("nombre", {
                   required: {
                     value: true,
                     message: "El campo es requerido",
+                  },
+                  onChange: (event) => {
+                    setNombre(event.target.value);
+                    console.log(nombre);
                   },
                 })}
                 error={Boolean(errors.nombre)}
@@ -170,10 +312,15 @@ export default function UserForm(props) {
                 id="apellidoid"
                 type="text"
                 label="Apellido"
+                value={apellido}
                 {...register("apellido", {
                   required: {
                     value: true,
                     message: "El campo es requerido",
+                  },
+                  onChange: (event) => {
+                    setApellido(event.target.value);
+                    console.log(apellido);
                   },
                 })}
                 error={Boolean(errors.apellido)}
@@ -187,71 +334,91 @@ export default function UserForm(props) {
                   control={<Checkbox />}
                   label="Laboratorio"
                   value="laboratorio"
+                  checked={roles.laboratorio}
                   {...register("roles")}
                 />
                 <FormControlLabel
                   control={<Checkbox />}
                   label="Recepción"
                   value="recepcion"
+                  checked={roles.recepcion}
                   {...register("roles")}
                 />
                 <FormControlLabel
                   control={<Checkbox />}
                   label="Secretaria"
                   value="secretaria"
+                  checked={roles.secretaria}
                   {...register("roles")}
                 />
                 <FormControlLabel
                   control={<Checkbox />}
                   label="Director"
                   value="director"
+                  checked={roles.director}
                   {...register("roles")}
                 />
                 <FormControlLabel
                   control={<Checkbox />}
                   label="Admin"
                   value="admin"
+                  checked={roles.admin}
                   {...register("roles")}
                 />
                 <FormControlLabel
                   control={<Checkbox />}
                   label="Médico"
                   value="medico"
-                  {...register("roles")}
+                  checked={roles.medico}
+                  {...register("roles", {
+                    onChange: (event) => {
+                      roles[event.target.value] = event.target.checked;
+                      setRoles({ ...roles });
+                    },
+                  })}
                 />
               </div>
             </Box>
           </Grid>
           <Grid item xs={6}>
             <Box className="fieldsContainer">
-              <TextField
-                id="contraseniaid1"
-                onChange="contraseniaid1"
-                type="password"
-                //pattern=".{6}"
-                label="Contraseña"
-                {...register("contrasenia", {
-                  required: {
-                    value: true,
-                    message: "El campo es requerido",
-                  },
-                  minLength: {
-                    value: 6,
-                    message: "La contraseña debe tener al menos 6 caracteres",
-                  },
-                })}
-                error={Boolean(errors.contrasenia)}
-                helperText={errors.contrasenia?.message}
-                sx={{ mt: "8px", mb: "4px", width: "100%" }}
-              />
-              <TextField
-                id="confirmContrasenia"
-                type="password"
-                label="Confirmar contraseña"
-                error={probar.show}
-                helperText={probar.message}
-                sx={{ mt: "8px", mb: "4px", width: "100%" }}
-              />
+              {!id && (
+                <>
+                  <TextField
+                    id="contraseniaid1"
+                    //onChange="contraseniaid1"
+                    type="password"
+                    //pattern=".{6}"
+                    label="Contraseña"
+                    {...register("contrasenia", {
+                      required: {
+                        value: id ? false : true,
+                        message: "El campo es requerido",
+                      },
+                      minLength: {
+                        value: 6,
+                        message:
+                          "La contraseña debe tener al menos 6 caracteres",
+                      },
+                      onChange: (event) => {
+                        setContrasenia(event.target.value);
+                        console.log(contrasenia);
+                      },
+                    })}
+                    error={Boolean(errors.contrasenia)}
+                    helperText={errors.contrasenia?.message}
+                    sx={{ mt: "8px", mb: "4px", width: "100%" }}
+                  />
+                  <TextField
+                    id="confirmContrasenia"
+                    type="password"
+                    label="Confirmar contraseña"
+                    error={mensajeContrasenia.show}
+                    helperText={mensajeContrasenia.message}
+                    sx={{ mt: "8px", mb: "4px", width: "100%" }}
+                  />
+                </>
+              )}
               <Button
                 type="submit"
                 variant="contained"
