60f25369f82f39a36c3d04866b40c6d26b8e6f7b
FrancoMostafa <francom2342@gmail.com>
2022-05-04 19:58:29 -0300

    acomodando app.js

--- turnero-backend/app.js
+++ turnero-backend/app.js
@@ -1,22 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const fileUpload = require("express-fileupload");
 var bodyParser = require("body-parser");
 const FeriadoController = require("./controllers/feriadoController");
 const LugarController = require("./controllers/lugar-controller");
 const TurnoController = require("./controllers/turno-controller");
+
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 let feriadoController = new FeriadoController();
 let lugarController = new LugarController();
 let turnoController = new TurnoController();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.use("/places", lugarController.router);
 app.use("/turns", turnoController.router);
 app.use("/feriado", feriadoController.router);
 
-module.exports = app;
 module.exports = app;
