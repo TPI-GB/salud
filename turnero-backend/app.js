16c9037f1e59a1ba6cfed004aabb2670d5078f2a
FrancoMostafa <francom2342@gmail.com>
2022-05-04 19:30:18 -0300

    api turnos v1

--- turnero-backend/app.js
+++ turnero-backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const lugarController = require("./controllers/lugar-controller");
+const turnoController = require("./controllers/turno-controller");
 
 app.use(cors());
 
@@ -9,5 +10,6 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/places", lugarController.router);
+app.use("/turns", turnoController.router);
 
 module.exports = app;
